feda4df06e683ee9bb5cfdde4f8f3ca2bc05977a
C261294 <C261294@TR-5D5K2Z2>
2020-06-20 10:24:50 -0700

    pks

---
+++ pankaj/src/pks/Pk1.java
@@ -0,0 +1,9 @@
+package pks;
+
+public class Pk1 {
+
+	public static void main(String[] args) {
+		System.out.println("hvuyfyufuyfvfuy");
+	}
+
+}
---
+++ pankaj/src/pks/Pk2.java
@@ -0,0 +1,10 @@
+package pks;
+
+public class Pk2 {
+
+	public static void main(String[] args) {
+	System.out.println("jftuftdytdcyt");
+
+	}
+
+}
